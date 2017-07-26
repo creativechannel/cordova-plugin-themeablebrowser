8d708df41d8ae22e7fd2bcc8bac0eb1a507231a6
Tim O'Meara <tim.omeara@gmail.com>
2017-07-26 14:50:51 -0500

    killing the nav bar and adding horizontal option, i hope

--- src/android/ThemeableBrowser.java
+++ src/android/ThemeableBrowser.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package android;
+package com.initialxy.cordova.themeablebrowser;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
